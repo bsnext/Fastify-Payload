458764b683a195b16c0da0736ec3d90a2055e62f
kashtesov <beststudiomta@gmail.com>
2024-12-06 06:12:07 +1000

    Update index.ts
    Да, но ещё нет...

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import { FastifyInstance, FastifyRequest, FastifyContextConfig, RouteShorthandOptions } from "fastify";
+import { FastifyInstance, FastifyRequest, FastifyBodyParser, FastifyError } from "fastify";
 import FastifyPlugin from "fastify-plugin";
-
 
 ////////////////////////////////
 
 export interface PluginOptions<T> {
 	global?: boolean;
 	paths?: string[];
-	parse?: `string` | `buffer`;
+	parse?: (T extends Buffer ? "buffer" : "string");
 	contentTypes?: {
-		[key: string]: 'default' | 
+		[key: string]: 'default' |
 		((request: FastifyRequest, payload: T, callback: (err: Error | null, result?: any) => void) => void);
 	};
 	overwriteWarning?: boolean;
@@ -30,7 +29,7 @@ declare module "fastify" {
 ////////////////////////////////
 
 export default FastifyPlugin(
-	function <T extends string | Buffer = string>(fastify: FastifyInstance, initOptions: PluginOptions<T>, done) {
+	function<T extends string | Buffer = string> (fastify: FastifyInstance, initOptions: PluginOptions<T>, done: (error?: FastifyError) => void) {
 		const options = {
 			global: (typeof initOptions.global === `boolean`) ? initOptions.global : false,
 			paths: new Set(initOptions.paths || []),
@@ -42,7 +41,7 @@ export default FastifyPlugin(
 			overwriteWarning: (typeof initOptions.overwriteWarning === `boolean`) ? initOptions.overwriteWarning : true
 		};
 
-		fastify.addHook(`onRoute`, 
+		fastify.addHook(`onRoute`,
 			function (routeOptions) {
 				if (routeOptions.config?.payload) {
 					options.paths.add(routeOptions.url);
@@ -51,23 +50,26 @@ export default FastifyPlugin(
 		);
 
 		////////////////////////////////
-
-		function getDefaultParser(contentType) {
+		
+		function getDefaultParser(contentType): FastifyBodyParser<string | Buffer> {
 			if (contentType === `application/json`) {
 				return fastify.getDefaultJsonParser(fastify.initialConfig.onProtoPoisoning || `error`, fastify.initialConfig.onConstructorPoisoning || `error`);
 
 			} else if (contentType === `text/plain`) {
-				return function (request, payload: T, callback) {
+				return function (request, payload, callback) {
 					callback(null, typeof payload === `string` ? payload : payload.toString());
 				};
-			} else if (contentType === `*`) { 
-				return function (request, payload: T, callback) {
+
+			} else if (contentType === `*`) {
+				return function (request, payload, callback) {
 					callback(null, payload);
 				};
 			};
 
 			throw new Error(`Content-type '${contentType}' not have a default parser!`);
 		}
+
+		////////////////////////////////
 
 		for (const [optionsContentType, optionsParser] of Object.entries(options.contentTypes)) {
 			if (typeof optionsParser !== `function` && optionsParser !== `default`) {
@@ -83,19 +85,14 @@ export default FastifyPlugin(
 				}
 			};
 
-			// Какая-то беда с options.parse. Оно может быть или 'buffer' или 'string'
-			// Ругается, потому-что что-то где-то указано не явно
-			// Не соображу, но это надо обязательно поправить (а я забил)
-			// @ts-ignore
-			fastify.addContentTypeParser(optionsContentType, { parseAs: options.parse },
+			fastify.addContentTypeParser<T>(optionsContentType, { parseAs: options.parse as (T extends Buffer ? "buffer" : "string") },
 				function (req, payload: T, done) {
 					const url = req.routeOptions.url;
 
 					if (url && (options.global || options.paths.has(url))) {
-						req.payload = payload;
+						req.payload = payload as T;
 					}
 
-					// @ts-ignore
 					parser(req, payload,
 						function (err, result) {
 							done(err, result);
